6e71f1402d4fffe558d014309cac8c51dce5c0bf
keeeal <j@keal.au>
2024-04-15 16:00:18 +0930

    use sphinx-rtd-theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,13 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Project information
 
 project = 'cq-extrusions'
 copyright = '2024, James Keal'
 author = 'James Keal'
 release = '0.0.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# -- General configuration
 
 extensions = [
     "autodoc2",
@@ -22,13 +17,6 @@ autodoc2_packages = [
     "../src/cq_extrusions",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# -- Options for HTML output
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
